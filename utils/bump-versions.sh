01214b19d243afbc1e90760eea1c0c69321cbb05
Matej Kollar <208115@mail.muni.cz>
2015-10-03 12:18:51 +0200

    Polish version-bumping script

--- utils/bump-versions.sh
+++ utils/bump-versions.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-manifest="../metadata/manifest.yml"
+declare -r manifest="../metadata/manifest.yml"
+declare -r binsetup="../bin/setup"
 
 cd "$( dirname "${0}" )"
 cd "$( pwd -P )"
 
-function getVersion() {
+function getCartridgeVersion() {
     sed -n 's/^Cartridge-Version:[^0-9.]*\([0-9.]*\)[^0-9.]*$/\1/p' "${manifest}"
 }
 
-oldVersion=$( getVersion )
+oldVersion=$( getCartridgeVersion )
 
 wget -q -O - "https://dist.torproject.org/" \
 | sed -n 's/.*>tor-\([0-9.]*\).tar.gz<.*/\1/p' \
 | ./manifestgen/dist/build/manifestgen/manifestgen "${oldVersion}" \
 > "${manifest}"
 
-git commit -v ../metadata/manifest.yml
-git tag "tor-openshift-$( getVersion )"
+latestTorVersion=$( sed -n 's/^Version: '\''\(.*\)'\''/\1/p' "${manifest}" )
+sed -i 's/^\(declare version=\).*/\1"'"${latestTorVersion}"'"/' "${binsetup}"
+
+newVersion=$( getCartridgeVersion )
+
+git commit -v "${manifest}" "${binsetup}" \
+&& git tag "tor-openshift-${newVersion}"
